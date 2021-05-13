c53f249b3f4e24fbbc8ccee3e7e00fea607e4d00
Will <shaheenwill@gmail.com>
2021-05-13 16:56:38 -0400

    updated file structure

---
+++ Develop/server.js
@@ -0,0 +1,50 @@
+const express = require ("express");
+const path= require("path");
+const fs = require("fs");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(express.static("public"));
+
+// allows us to recieve data from post requ and retrieve data from body
+app.use(bodyParser.json());
+
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname,"./public/index.html"));        
+});
+
+app.get("/notes",(req,res)=>{
+    res.sendFile(path.join(__dirname,"./public/notes.html"));         
+});
+
+app.get("/api/notes",(req,res)=>{
+    res.json(getData());            
+});
+
+app.post("/api/notes",(req,res)=>{
+    const newNote=req.body;
+    newNote.id=Date.now();
+    const data=getData();
+    data.push(newNote);
+    saveData(data);
+    res.send();    
+});
+
+app.delete("/api/notes/:id",(req,res)=>{
+    const id=req.params.id;
+    const data=getData();
+    const index=data.findIndex(note => note.id===id);
+    if (index>=0)data.splice(index,1);
+    res.send();               
+});
+
+app.listen(3000, () =>console.log("Express listening on port 3000"));
+
+function getData(){
+    const text =fs.readFileSync("./db/db.json");
+    return JSON.parse(text);
+}
+
+function saveData(data) {
+    fs.writeFileSync("./db/db.json",JSON.stringify(data));    
+}
